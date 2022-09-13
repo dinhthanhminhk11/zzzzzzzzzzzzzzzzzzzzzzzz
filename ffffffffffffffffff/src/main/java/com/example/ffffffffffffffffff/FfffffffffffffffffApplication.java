1421326c2035b7e2e398e52755c073689ca997fe
minhdinh <minhdinhthanh@vccorp.vn>
2022-09-13 17:14:08 +0700

    first commit

---
+++ ffffffffffffffffff/src/main/java/com/example/ffffffffffffffffff/FfffffffffffffffffApplication.java
@@ -0,0 +1,13 @@
+package com.example.ffffffffffffffffff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FfffffffffffffffffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FfffffffffffffffffApplication.class, args);
+    }
+
+}
